d25bba303d4e856f182f66be51c2bdf15b4e7f53
Joao Paulo <jpmais2005@gmail.com>
2023-02-17 17:25:10 -0300

    Initial Commit

---
+++ Fala foundantion/Fala_foundantionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fala_foundantionApp.swift
+//  Fala foundantion
+//
+//  Created by user on 17/02/23.
+//
+
+import SwiftUI
+
+@main
+struct Fala_foundantionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
